06300edd7fd37552205ad19c9b494aded32e25cc
EveSquare <50450074+EveSquare@users.noreply.github.com>
2020-12-31 18:39:54 +0900

    a

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, abort
 import random
+from Crypto.Cipher import AES
  
 from linebot import (
     LineBotApi, WebhookHandler
@@ -16,6 +17,8 @@ from linebot.models import (
 import os
  
 app = Flask(__name__)
+
+key = b"J+MGhAqNuH+wV.!8"
  
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
@@ -49,54 +52,59 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     # print(f'event.reply_token:{event.reply_token}')
+    cipher = AES.new(key, AES.MODE_EAX)
+    ciphertext, tag = cipher.encrypt_and_digest(event.message.text)
 
-    # profile = line_bot_api.get_profile(event.source.user_id)
     line_bot_api.reply_message(
         event.reply_token,
-        TemplateSendMessage(
-        alt_text='Buttons template',
-        template=ButtonsTemplate(
-        thumbnail_image_url='https://example.com/image.jpg',
-        title='Menu',
-        text='Please select',
-        actions=[
-            PostbackAction(
-                label='postback',
-                display_text='postback text',
-                data='action=buy&itemid=1'
-            ),
-            MessageAction(
-                label='message',
-                text='message text'
-            ),
-            URIAction(
-                label='uri',
-                uri='http://example.com/'
-            )
-        ]
-    )
-))
+        TextSendMessage(text=ciphertext))
+    # profile = line_bot_api.get_profile(event.source.user_id)
+#     line_bot_api.reply_message(
+#         event.reply_token,
+#         TemplateSendMessage(
+#         alt_text='Buttons template',
+#         template=ButtonsTemplate(
+#         thumbnail_image_url='https://example.com/image.jpg',
+#         title='Menu',
+#         text='Please select',
+#         actions=[
+#             PostbackAction(
+#                 label='postback',
+#                 display_text='postback text',
+#                 data='action=buy&itemid=1'
+#             ),
+#             MessageAction(
+#                 label='message',
+#                 text='message text'
+#             ),
+#             URIAction(
+#                 label='uri',
+#                 uri='http://example.com/'
+#             )
+#         ]
+#     )
+# ))
 
-    line_bot_api.reply_message(
-            event.reply_token,
-            TemplateSendMessage(
-                alt_text='Confirm template',
-                template=ConfirmTemplate(
-                    text='Are you sure?',
-                    actions=[
-                        PostbackTemplateAction(
-                            label='Yes',
-                            text='postback text',
-                            data="> Yes"
-                        ),
-                        MessageTemplateAction(
-                            label='No',
-                            text='> No'
-                        )
-                    ]
-                )
-            )
-        )
+#     line_bot_api.reply_message(
+#             event.reply_token,
+#             TemplateSendMessage(
+#                 alt_text='Confirm template',
+#                 template=ConfirmTemplate(
+#                     text='Are you sure?',
+#                     actions=[
+#                         PostbackTemplateAction(
+#                             label='Yes',
+#                             text='postback text',
+#                             data="> Yes"
+#                         ),
+#                         MessageTemplateAction(
+#                             label='No',
+#                             text='> No'
+#                         )
+#                     ]
+#                 )
+#             )
+#         )
 
 
 @handler.add(MessageEvent, message=StickerMessage)
